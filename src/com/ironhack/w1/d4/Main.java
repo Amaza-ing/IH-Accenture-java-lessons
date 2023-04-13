178a29eeae69db8f38c27a0d660e322fe6f65bb2
Adrián Maza <>
2023-04-13 11:28:55 +0200

    big decimal added

--- src/com/ironhack/w1/d4/Main.java
+++ src/com/ironhack/w1/d4/Main.java
@@ -1,10 +1,13 @@
 package com.ironhack.w1.d4;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
+
+//        ABSTRACT CLASSES AND INTERFACES
+
         Player player = new WebPlayer(0.5, 1);
         WebPlayer webPlayer = new WebPlayer(0.3, 2);
 
@@ -16,7 +19,24 @@ public class Main {
         IPlayable androidPlayer2 = new AndroidPlayer(0.5, 4);
         androidPlayer.close();
         androidPlayer.play();
-        androidPlayer2.play();
 //        androidPlayer2.close(); // esto no se puede hacer porque es de tipo IPlayable
+        androidPlayer2.play();
+
+
+//        BIG DECIMAL
+
+        System.out.println(1.2 - 1.0);
+        BigDecimal bd1 = new BigDecimal("31.237234234");
+        BigDecimal bd2 = new BigDecimal("12.345324503");
+
+        System.out.println(bd1.subtract(bd2));
+        System.out.println(bd1.add(bd2));
+        System.out.println(bd1.multiply(bd2));
+        System.out.println(bd1.divide(bd2, RoundingMode.HALF_EVEN));
+        System.out.println(bd1.setScale(2, RoundingMode.HALF_EVEN));
+        System.out.println(bd1.compareTo(bd2));
+        if (bd1.compareTo(bd2) > 0) System.out.println("bd1 es mayor");
+        else if (bd1.compareTo(bd2) < 0) System.out.println("bd1 es menor");
+        else if (bd1.compareTo(bd2) == 0) System.out.println("bd1 es igual a bd2");
     }
 }
